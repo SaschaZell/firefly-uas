225cc1f1d60cb8e31ae9a8b98bd5bcc8bf69e1ae
SaschaZell <SaschaZell@users.noreply.github.com>
2024-12-19 17:22:53 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="SaschaZell"
+original_name="firefly_uas"
+original_urlname="firefly-uas"
+original_description="Awesome firefly_uas created by SaschaZell"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv firefly_uas $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
